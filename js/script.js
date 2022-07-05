4a8423c697c32cf1f4925a380f4968fa6cd1b685
chisom <christianezeanyaeche@gmail.com>
2022-07-05 11:53:02 -0400

    added style and functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
 const LOCALSTORAGE_KEY = 'books';
 const myLibrary = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || [];
-const form = document.querySelector('form')
-
+const form = document.querySelector('form');
+const showFormBtn = document.querySelector('header .head button');
+const mode = {edit: false, index: null}
 renderList(myLibrary);
 
+
+showFormBtn.addEventListener('click', () => {
+  form.classList.toggle('show');
+  if(form.classList.contains('show')) {
+    showFormBtn.textContent = 'x'
+  } else{
+    showFormBtn.textContent = '+'
+    mode.edit = false;
+    mode.index = null;
+
+    const author = document.querySelector('#author');
+    const title = document.querySelector('#title');
+    const numOfPages = document.querySelector('#num__of__pages');
+    const readStatus = document.querySelector('#read__status');
+    author.value = '';
+    title.value = '';
+    numOfPages.value = '';
+    readStatus.checked = false;
+  };
+})
 
 function Book(title,author,numOfPages,isRead) {
    this.title = title;
@@ -21,23 +42,67 @@ const createBook = ( title, author, numOfPages, isRead) => {
 }
 
 form.addEventListener( 'submit', (e) => {
-  e.preventDefault()
-  const author = document.querySelector('#author');
-  const title = document.querySelector('#title');
-  const numOfPages = document.querySelector('#num__of__pages');
-  const readStatus = document.querySelector('#read__status');
-  addBookToLibrary(createBook(title.value, author.value, Number(numOfPages.value), readStatus.checked));
-  author.value = '';
-  title.value = '';
-  numOfPages.value = '';
-  readStatus.checked = false;
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(myLibrary));
-  renderList(myLibrary);
+  e.preventDefault();
+  if (mode.edit) {
+    const author = document.querySelector('#author');
+    const title = document.querySelector('#title');
+    const numOfPages = document.querySelector('#num__of__pages');
+    const readStatus = document.querySelector('#read__status');
+
+    const index = mode.index;
+    myLibrary[index].title = title.value;
+    myLibrary[index].author = author.value;
+    myLibrary[index].numOfPages = numOfPages.value;
+    myLibrary[index].readStatus = readStatus.value;
+
+    author.value = '';
+    title.value = '';
+    numOfPages.value = '';
+    readStatus.checked = false;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(myLibrary));
+    renderList(myLibrary);
+    form.classList.remove('show');
+    showFormBtn.textContent = '+';
+    mode.edit = false;
+    mode.index = null;
+  } else {
+    const author = document.querySelector('#author');
+    const title = document.querySelector('#title');
+    const numOfPages = document.querySelector('#num__of__pages');
+    const readStatus = document.querySelector('#read__status');
+
+    addBookToLibrary(createBook(title.value, author.value, Number(numOfPages.value), readStatus.checked));
+
+    author.value = '';
+    title.value = '';
+    numOfPages.value = '';
+    readStatus.checked = false;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(myLibrary));
+    renderList(myLibrary);
+    form.classList.remove('show');
+    showFormBtn.textContent = '+';
+    mode.edit = false;
+    mode.index = null;
+  }
+  
 })
 
 function renderList(listArr) {
-  const list = document.querySelector('ul.card__grid')
+  const list = document.querySelector('ul.card__grid');
+  if(document.querySelector('.empty__list')){
+    const emptyList = document.querySelector('.empty__list');
+    document.querySelector('main').removeChild(emptyList)
+  }
+ 
   list.innerHTML = '';
+  if (listArr.length < 1){
+    const emptyList = document.createElement('div');
+    emptyList.setAttribute('class', 'empty__list')
+    const msg =  document.createElement('p');
+    msg.textContent = 'Your Library is currently empty, click the add button above to add books';
+    emptyList.appendChild(msg);
+    return document.querySelector('main').appendChild(emptyList)
+  }
   return listArr.map(
     (item) => {
       const listItem = createCard(item);
@@ -46,7 +111,6 @@ function renderList(listArr) {
       return list.appendChild(li)
     }
   )
-
 }
 
 function createCard(book) {
@@ -56,25 +120,31 @@ function createCard(book) {
   const numOfPages = document.createElement('div');
   const readStatus = document.createElement('div');
   const deleteBtn = document.createElement('div');
+  deleteBtn.setAttribute('class', 'delete__edit');
   
   card.setAttribute('class', 'card');
   title.setAttribute('class', 'title');
   author.setAttribute('class', 'author');
   numOfPages.setAttribute('class', 'number__of__pages');
   readStatus.setAttribute('class', 'read__status');
-  deleteBtn.setAttribute('class', 'delete');
+ 
 
   const titleContent = document.createElement('h2');
   titleContent.textContent = book.title;
 
   const authorContent = document.createElement('p');
-  authorContent.textContent = book.author;
+  authorContent.textContent = 'author : ' + book.author;
 
   const numOfPagesContent = document.createElement('p');
-  numOfPagesContent.textContent = book.numOfPages;
+  numOfPagesContent.textContent = 'pages : ' + book.numOfPages;
 
-  const button = document.createElement('button');
-  button.textContent = 'Delete';
+  const buttonDel = document.createElement('button');
+  buttonDel.setAttribute('class', 'delete');
+  buttonDel.textContent = 'Delete';
+
+  const buttonEdit = document.createElement('button');
+  buttonEdit.setAttribute('class', 'edit');
+  buttonEdit.textContent = 'Edit';
 
   title.appendChild(titleContent);
   author.appendChild(authorContent);
@@ -82,17 +152,27 @@ function createCard(book) {
 
   const checkbox = document.createElement('input');
   checkbox.setAttribute('type', 'checkbox');
-  if(book.isRead){
+  if(book.isRead){  
     checkbox.setAttribute('checked', '');
   }
+
+  checkbox.addEventListener('change', (e) => {
+    const list = document.querySelectorAll('ul.card__grid > li'); 
+    const parent = e.target.parentElement.parentElement.parentElement;
+    const index = [...list].findIndex(item => item === parent );
+    myLibrary[index].isRead = !myLibrary[index].isRead;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(myLibrary));
+  })
+
   const readContent = document.createElement('p');
   readContent.textContent = 'read';
 
   readStatus.appendChild(readContent);
   readStatus.appendChild(checkbox);
 
-  deleteBtn.appendChild(button);
-  deleteBtn.addEventListener('click', (e) => { 
+  deleteBtn.appendChild(buttonDel);
+  deleteBtn.appendChild(buttonEdit);
+  deleteBtn.querySelector('.delete').addEventListener('click', (e) => { 
     const list = document.querySelectorAll('ul.card__grid > li'); 
     const parent = e.target.parentElement.parentElement.parentElement;
     const index = [...list].findIndex(item => item === parent );
@@ -100,12 +180,32 @@ function createCard(book) {
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(myLibrary))
     renderList(myLibrary);
   });
+  deleteBtn.querySelector('.edit').addEventListener('click', (e) => { 
+    mode.edit = true;
+    showFormBtn.textContent = 'x'
+    const list = document.querySelectorAll('ul.card__grid > li'); 
+    const parent = e.target.parentElement.parentElement.parentElement;
+    const index = [...list].findIndex(item => item === parent );
+    mode.index = index;
+    form.classList.add('show');
+    const author = document.querySelector('#author');
+    const title = document.querySelector('#title');
+    const numOfPages = document.querySelector('#num__of__pages');
+    const readStatus = document.querySelector('#read__status');
+    author.value = myLibrary[index].author;
+    title.value = myLibrary[index].title;
+    numOfPages.value = myLibrary[index].numOfPages;
+    readStatus.checked = myLibrary[index].isRead;
+
+
+  });
 
   card.appendChild(title);
   card.appendChild(author);
   card.appendChild(numOfPages);
   card.appendChild(readStatus);
   card.appendChild(deleteBtn);
+
   return card;
 };
 
